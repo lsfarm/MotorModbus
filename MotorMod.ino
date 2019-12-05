90edd04b1ce166d51c9f94ae1aee214db5ef8de9
LSTexasFarmer <58246081+LSTexasFarmer@users.noreply.github.com>
2019-12-05 12:14:53 -0600

    Update MotorMod.ino
    MotorModV2

--- MotorMod.ino
+++ MotorMod.ino
@@ -9,6 +9,8 @@
 #define MAX485_DE      22
 #define MAX485_RE_NEG  23
 
+#define MAX_TRIES 5
+
 // instantiate ModbusMaster object
 ModbusMaster node;
 
@@ -54,19 +56,24 @@ typedef union {
 void loop()
 {
   readTest();
+  delay(2000);
 }
 
 void readTest() {
   uint8_t result;
   uint16_t data;
-  
+  uint8_t tries = 0;
+
   // Read coils at function address FC(0x01) Register(10001) Address(0) Hex(0x0000) (Read-Only Status Bits)
-  Serial.println("Reading status bits...");
-  result = node.readCoils(0, 24);
+  Serial.println("Reading status bits...");  
+  while (tries < MAX_TRIES && result != node.ku8MBSuccess) {
+    result = node.readCoils(0, 8);
+    tries++;
+  }
   // Not 100% on the order of bits
   if (result == node.ku8MBSuccess) {
     data = node.getResponseBuffer(0);
-    Serial.print("Raw data: ");Serial.println(data);
+    Serial.print("1st Raw data: ");Serial.println(data);
     Serial.print("Syncing: ");Serial.println(bitRead(data, 0));
     Serial.print("InSync1: ");Serial.println(bitRead(data, 1));
     Serial.print("InSync2: ");Serial.println(bitRead(data, 2));
@@ -75,17 +82,48 @@ void readTest() {
     Serial.print("Firing: ");Serial.println(bitRead(data, 5));
     Serial.print("LockOut: ");Serial.println(bitRead(data, 6));
     Serial.print("FIRED: ");Serial.println(bitRead(data, 7));
-    Serial.print("Cranking: ");Serial.println(bitRead(data, 8));
-    Serial.print("Running: ");Serial.println(bitRead(data, 9));
-    Serial.print("Wrong Disk: ");Serial.println(bitRead(data, 10));
-    Serial.print("GLead Shutdown Grounded: ");Serial.println(bitRead(data, 11));
-    Serial.print("Remote Shutdown Present: ");Serial.println(bitRead(data, 12));
-    Serial.print("GLead Shutdown Logged: ");Serial.println(bitRead(data, 13));
-    Serial.print("Remote Shutdown Logged: ");Serial.println(bitRead(data, 14));
-    Serial.print("Overspeed Shutdown Logged: ");Serial.println(bitRead(data, 15));
-    
-    data = node.getResponseBuffer(1);
-    Serial.print("Raw data: ");Serial.println(data);
+  } else {
+    Serial.println("Failed to read 1st set of coils");
+    Serial.print("Result: ");Serial.println(result);
+  }
+  
+  delay(1000);
+  Serial.flush();
+
+  tries = 0;
+  while (tries < MAX_TRIES && result != node.ku8MBSuccess) {
+    result = node.readCoils(8, 8);
+    tries++;
+  }
+
+  if (result == node.ku8MBSuccess) {
+    data = node.getResponseBuffer(0);
+    Serial.print("2nd Raw data: ");Serial.println(data);
+    Serial.print("Cranking: ");Serial.println(bitRead(data, 0));
+    Serial.print("Running: ");Serial.println(bitRead(data, 1));
+    Serial.print("Wrong Disk: ");Serial.println(bitRead(data, 2));
+    Serial.print("GLead Shutdown Grounded: ");Serial.println(bitRead(data, 3));
+    Serial.print("Remote Shutdown Present: ");Serial.println(bitRead(data, 4));
+    Serial.print("GLead Shutdown Logged: ");Serial.println(bitRead(data, 5));
+    Serial.print("Remote Shutdown Logged: ");Serial.println(bitRead(data, 6));
+    Serial.print("Overspeed Shutdown Logged: ");Serial.println(bitRead(data, 7));
+  } else {
+    Serial.println("Failed to read 2nd set of coils");
+    Serial.print("Result: ");Serial.println(result);
+  }
+  
+  delay(1000);
+  Serial.flush();
+
+  tries = 0;
+  while (tries < MAX_TRIES && result != node.ku8MBSuccess) {
+    result = node.readCoils(16, 8);
+    tries++;
+  }
+
+  if (result == node.ku8MBSuccess) {
+    data = node.getResponseBuffer(0);
+    Serial.print("3rd Raw data: ");Serial.println(data);
     Serial.print("WDOG1 Reset Latched: ");Serial.println(bitRead(data, 0));
     Serial.print("WDOG2 Reset Event: ");Serial.println(bitRead(data, 1));
     Serial.print("CheckSum Error: ");Serial.println(bitRead(data, 2));
@@ -95,19 +133,23 @@ void readTest() {
     Serial.print("Shorted Primary: ");Serial.println(bitRead(data, 6));
     Serial.print("Open Secondary: ");Serial.println(bitRead(data, 7));
   } else {
-    Serial.println("Failed to read coils");
+    Serial.println("Failed to read 3rd set of coils");
     Serial.print("Result: ");Serial.println(result);
   }
 
-  delay(5000);
+  delay(1000);
+  Serial.flush();
 
   // Read the 16 registers at function address FC(0x03) Register(30005) Address(4) HEX(0x0004) (Read only status registers starting with RPM)
   Serial.println("Reading read only status registers...");
-  result = node.readHoldingRegisters(4, 16);
-
-  delay(2000);
   
-  if (result == node.ku8MBSuccess) {  
+  tries = 0;
+  while (tries < MAX_TRIES && result != node.ku8MBSuccess) {
+    result = node.readHoldingRegisters(4, 8);
+    tries++;
+  }
+
+  if (result == node.ku8MBSuccess) {
     Serial.print("RPM: ");Serial.println(node.getResponseBuffer(0));
     Serial.print("Timing: ");Serial.print(((signed)node.getResponseBuffer(1)) / 10.0);Serial.println(" DEG");
     Serial.print("Switch Position: ");Serial.println(node.getResponseBuffer(2));
@@ -116,6 +158,21 @@ void readTest() {
     Serial.print("Insertion Retard: ");Serial.print(node.getResponseBuffer(5) / 10.0);Serial.println(" DEG");
     Serial.print("Switch Retard: ");Serial.print(node.getResponseBuffer(6) / 10.0);Serial.println(" DEG");
     Serial.print("Loop Retard: ");Serial.print(node.getResponseBuffer(7) / 10.0);Serial.println(" DEG");
+  } else {
+    Serial.println("Failed to read 1st set read-only status registers!");
+    Serial.print("Result: ");Serial.println(result);
+  }
+
+  delay(1000);
+  Serial.flush();
+
+  tries = 0;
+  while (tries < MAX_TRIES && result != node.ku8MBSuccess) {
+    result = node.readHoldingRegisters(12, 8);
+    tries++;
+  }
+
+  if (result == node.ku8MBSuccess) {  
     Serial.print("RPM Retard: ");Serial.print(node.getResponseBuffer(8) / 10.0);Serial.println(" DEG");
     Serial.print("Total Retard: ");Serial.print(node.getResponseBuffer(9) / 10.0);Serial.println(" DEG");
     Serial.print("Cycle Counter HI: ");Serial.println(node.getResponseBuffer(10));
@@ -125,15 +182,21 @@ void readTest() {
     Serial.print("Spark Ref. Num. Output 2: ");Serial.println(node.getResponseBuffer(14));
     Serial.print("Spark Ref. Num. Output 3: ");Serial.println(node.getResponseBuffer(15));
   } else {
-    Serial.println("Failed to read read-only status registers!");
+    Serial.println("Failed to read 2nd set read-only status registers!");
     Serial.print("Result: ");Serial.println(result);
   }
 
-  delay(5000);
+  delay(1000);
+  Serial.flush();
   
   // Read 8 read/write registers starting at function address FC(0x04) Register(40005) Address(4) HEX(0x0004)
   Serial.println("Reading read/write input registers...");
-  result = node.readInputRegisters(4, 8);
+
+  tries = 0;
+  while (tries < MAX_TRIES && result != node.ku8MBSuccess) {
+    result = node.readInputRegisters(4, 8);
+    tries++;
+  }
 
   if (result == node.ku8MBSuccess) {
     Serial.print("Disk+1: ");
@@ -169,11 +232,17 @@ void readTest() {
         Serial.print("Result: ");Serial.println(result);
   }
   
-  delay(5000);
+  delay(1000);
+  Serial.flush();
 
     // Read 7 misc read/write registers starting at function address FC(0x04) Register(40122) Address(121) HEX(0x0079)
   Serial.println("Reading misc read/write input registers...");
-  result = node.readInputRegisters(121, 7);
+
+  tries = 0;
+  while (tries < MAX_TRIES && result != node.ku8MBSuccess) {
+    result = node.readInputRegisters(121, 7);
+    tries++;
+  }
 
   if (result == node.ku8MBSuccess) {
     Serial.print("Crank Counter: ");
@@ -205,5 +274,6 @@ void readTest() {
         Serial.print("Result: ");Serial.println(result);
   }
   
-  delay(5000);
+  delay(1000);
+  Serial.flush();
 }
